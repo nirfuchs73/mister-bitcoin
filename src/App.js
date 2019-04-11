3036dd6b3642f863983a74548fe20e6c55fa25b8
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-04-11 15:12:28 +0300

    StatisticPage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 
 import HomePage from './pages/HomePage';
+import StatisticPage from './pages/StatisticPage';
+import ContactPage from './pages/ContactPage';
 
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                {/* <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Edit <code>src/App.js</code> and save to reload.</p>
-                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-                </header> */}
-                <HomePage />
+                {/* <HomePage /> */}
+                {/* <StatisticPage /> */}
+                <ContactPage />
             </div>
         );
     }
