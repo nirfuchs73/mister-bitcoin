3f6050a3d2acbbfbebf483e0dfb63fe61a42beb1
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-04-22 12:30:00 +0300

    Updates

---
+++ precache-manifest.da0253e7317d28b1de21eb5e5dcdbfca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9590c752a00658eae37c",
+    "url": "/mister-bitcoin/static/js/runtime~main.173c7f6e.js"
+  },
+  {
+    "revision": "ff8828f46c02d47929b0",
+    "url": "/mister-bitcoin/static/js/main.207b07eb.chunk.js"
+  },
+  {
+    "revision": "bd1b80e3af5122be8d1e",
+    "url": "/mister-bitcoin/static/js/2.9003a3d0.chunk.js"
+  },
+  {
+    "revision": "ff8828f46c02d47929b0",
+    "url": "/mister-bitcoin/static/css/main.7c45cda4.chunk.css"
+  },
+  {
+    "revision": "1d4dbad39505d7a53428a42493de4536",
+    "url": "/mister-bitcoin/index.html"
+  }
+];
